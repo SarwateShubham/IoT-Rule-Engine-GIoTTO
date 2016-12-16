68b4350914f5aebbaaa6f721a7a891b860b1fe1d
Shubham Sarwate <sshubham369@gmail.com>
2016-12-15 23:06:46 -0500

    Update Actuator.js

--- GioTTo-Actuate/Actuator.js
+++ GioTTo-Actuate/Actuator.js
@@ -19,17 +19,17 @@ var result;
         this.type1=config.type1;
 	this.newstate=config.newstate;
 	this.on('input', function(msg) {
-	   new_state=msg.payload;
-	   result=Actuate(this.type1,new_state,this.identity);
-            msg.payload = {"value":this.identity,"role":this.type1,"new":new_state};
+	   newstate1=msg.payload;
+	   result=Actuate(this.type1,newstate1,this.identity);
+            msg.payload = {"value":this.identity,"role":this.type1,"new":newstate1};
            this.send(msg);
         });
     }
-	
 function Actuate(type,new_state,identity){
 var request = require('request');
+
 request.post(
-    'http://BUILDING_DEPOT_ADDRESS:AE_PORT/api',
+    'http://bd-exp.andrew.cmu.edu:69/api',
     { json: {type:type,new_state:String(new_state),identity:identity} },
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -40,4 +40,5 @@ request.post(
 }
     RED.nodes.registerType("GioTTo-Actuate",ActuateNode);
 }
+
 
