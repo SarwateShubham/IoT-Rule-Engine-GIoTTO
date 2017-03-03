47aa3869de457e8dc8c7b4d7854c89f6cefd97f8
Shubham Sarwate <sshubham369@gmail.com>
2017-03-03 19:21:15 +0530

    Added text-box for Location

--- GioTTo-Sensor/Sensor.js
+++ GioTTo-Sensor/Sensor.js
@@ -20,28 +20,32 @@ function RuleNode(config) {
     });
 }
 
-
 function get_values(uuid){
-
+  uuid='199e9c75-1ea7-4971-937d-ccc4673c5cf6';
   var end_time= new Date() / 1000;
-  var start_time=end_time-150;
+  var start_time=end_time-100;
   var access_token=null;
   var vals, data;
 var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", 'BD_oauth_address', false ); // false for synchronous request
     xmlHttp.send( null );
     data=JSON.parse(xmlHttp.responseText);
+    console.log(data);
     access_token=data.access_token;
 //    return access_token;
 
 var xml = new XMLHttpRequest();
-    xml.open("GET","BD_time_address"+uuid+"/timeseries?start_time="+String(start_time)+"&end_time="+String(end_time),false);
-    xml.setRequestHeader("Authorization", "bearer " + access_token );
-    xml.send( null );
+    url="https://bd-exp.andrew.cmu.edu:82/api/sensor/"+uuid+"/timeseries?start_time="+String(start_time)+"&end_time="+String(end_time)
+    //url='https://bd-exp.andrew.cmu.edu:82/api/sensor/199e9c75-1ea7-4971-937d-ccc4673c5cf6/timeseries?start_time=1488545905.22649&end_time=1488547405.22649'
+    xml.open("GET",url,false);
+    xml.setRequestHeader("Authorization", "Bearer " + access_token );
+    xml.send(null);
+    console.log(url);
     vals=JSON.parse(xml.responseText);
+    console.log(vals);
     recent_val=vals.data.series[0].values[vals.data.series[0].values.length-1][2];
+    console.log(recent_val);
     return  recent_val;
-
 }
 
 RED.nodes.registerType("GioTTo-Sensor",RuleNode);
